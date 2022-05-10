658c1094a6618f6a820ac3d0a73d5adcde7d809c
louisdupas <louisdupas_pro@pm.me>
2022-05-10 11:11:18 +0200

    post user route ok

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -25,4 +25,29 @@ router.get('/:id', (req, res) =>  {
     })
 })
 
+// Maintenant, j'aimerais pouvoir ajouter de la data dans ma DB
+// sur ce thème précis évidemment
+router.post('/', (req, res) => {
+    // Ici je déscructure le corps de ma requête
+    // Corps de ma requête = propriétés de ma table DB
+    // (au moins, ce qui est en NOT NULL par défaut)
+    const { email, password } = req.body;
+    connection.query(
+    'INSERT INTO user (email, password) VALUES (?, ?)',
+    [email, password],
+    (err, result) => {
+        if (err) {
+            res.status(500).send('Error retrieving products from database');
+        }
+        else 
+        {
+            const id = result.insertId;
+            // Ici je définis ce que je veux voir en tant que retour json
+            const createdUser = { id, email, password };
+            res.status(201).json(createdUser);
+        }
+    }
+)
+})
+
 module.exports = router;
