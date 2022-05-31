02b9ffa98f3c99393a7aec2a8946e0e894b2fd29
louisdupas <louisdupas_pro@pm.me>
2022-05-31 16:56:41 +0200

    encrypt, token et MVC ok

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,7 +1,18 @@
 const connection = require("../db-config");
 const router = require("express").Router();
+const Joi = require('joi');
+const argon2 = require('argon2');
 
-router.get('/', (req, res) => {
+const { findUserByEmail, insertUser } = require('../models/user');
+const { generateJwt } = require('../utils/auth');
+const checkJwt = require('../middlewares/checkJwt')
+
+const userSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+})
+
+router.get('/', checkJwt, (req, res) => {
     connection.query("SELECT * FROM user", (err, result) => {
         if(err) {
             res.status(500).send("Error retrieving users from database");
@@ -25,30 +36,86 @@ router.get('/:id', (req, res) =>  {
     })
 })
 
+router.post('/', async (req, res) => {
+    const { value, error } = userSchema.validate(req.body);
+    
+    if (error) {
+        return res.status(400).json(error);
+    }
+
+    const [[existingUser]] = await findUserByEmail(value.email);
+    if (existingUser) {
+        return res.status(409).json({
+            message: "L'utilisateur existe déjà.",
+        })
+    }
+
+    const hashedPassword = await argon2.hash(value.password);
+    await insertUser(value.email, hashedPassword);
+
+    // const jwtKey = generateJwt(value.email);
+    // return res.json({
+    //     credentials: jwtKey,
+    // })
+
+    return res.json({
+        message: "L'utilisateur a bien été créé"
+    })
+});
+
+router.post('/login', async (req, res) => {
+    const { value, error } = userSchema.validate(req.body);
+    
+    if (error) {
+      return res.status(400).json(error);
+    };
+  
+    const [[existingUser]] = await findUserByEmail(value.email);
+  
+    if (!existingUser) {
+      return res.status(403).json({
+        message: 'Utilisateur non trouvé ou le mdp ne correspond pas au compte.'
+      })
+    };
+  
+    const verified = await argon2.verify(existingUser.password, value.password)
+  
+    if (!verified) {
+      return res.status(403).json({
+        message: 'Utilisateur non trouvé ou le mdp ne correspond pas au compte.'
+      })
+    }
+  
+    const jwtKey = generateJwt(value.email);
+    return res.json({
+      credentials: jwtKey
+    })
+})
+
 // Maintenant, j'aimerais pouvoir ajouter de la data dans ma DB
 // sur ce thème précis évidemment
-router.post('/', (req, res) => {
-    // Ici je déscructure le corps de ma requête
-    // Corps de ma requête = propriétés de ma table DB
-    // (au moins, ce qui est en NOT NULL par défaut)
-    const { email, password } = req.body;
-    connection.query(
-    'INSERT INTO user (email, password) VALUES (?, ?)',
-    [email, password],
-    (err, result) => {
-        if (err) {
-            res.status(500).send('Error retrieving products from database');
-        }
-        else 
-        {
-            const id = result.insertId;
-            // Ici je définis ce que je veux voir en tant que retour json
-            const createdUser = { id, email, password };
-            res.status(201).json(createdUser);
-        }
-    }
-)
-});
+// router.post('/', (req, res) => {
+//     // Ici je déscructure le corps de ma requête
+//     // Corps de ma requête = propriétés de ma table DB
+//     // (au moins, ce qui est en NOT NULL par défaut)
+//     const { email, password } = req.body;
+//     connection.query(
+//     'INSERT INTO user (email, password) VALUES (?, ?)',
+//     [email, password],
+//     (err, result) => {
+//         if (err) {
+//             res.status(500).send('Error retrieving products from database');
+//         }
+//         else 
+//         {
+//             const id = result.insertId;
+//             // Ici je définis ce que je veux voir en tant que retour json
+//             const createdUser = { id, email, password };
+//             res.status(201).json(createdUser);
+//         }
+//     }
+// )
+// });
 
 // Maintenant, je veux pouvoir modifier des infos 
 // concernant un produit 
