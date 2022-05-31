1ebf44c9d270a1f900c1c10910370b5699128674
louisdupas <louisdupas_pro@pm.me>
2022-05-31 14:29:35 +0200

    language routes ok

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 const userRouter = require("./users.routes");
 const projectRouter = require("./projects.routes");
 const categoryRouter = require("./categories.routes");
+const languageRouter = require("./languages.routes");
 
 router.use('/users', userRouter);
 router.use('/projects', projectRouter);
 router.use('/categories', categoryRouter);
+router.use('/languages', languageRouter);
 
 module.exports = router;
