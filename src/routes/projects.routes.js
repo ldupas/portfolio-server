950c972b82c580f5f1d0ef62195d204cfd9ab00d
louisdupas <louisdupas_pro@pm.me>
2022-06-13 10:44:44 +0200

    upload multer back ok

--- src/routes/projects.routes.js
+++ src/routes/projects.routes.js
@@ -1,15 +1,26 @@
 const connection = require("../db-config");
 const router = require("express").Router();
+const multer = require('multer');
+const { findAll, insertProject } = require('../models/project');
 
-router.get('/', (req, res) => {
-    connection.query("SELECT * FROM project", (err, result) => {
-        if(err) {
-            res.status(500).send("Error retrieving projects from database");
-        } else {
-            res.json(result);
-        }
-    })
-});
+// ici je définis le dossier de destination des fichiers de la sous-catégorie projects
+// cela va créer automatiquement un dossier projects dans le dossier uploads
+const upload = multer({ dest: 'uploads/projects/'})
+
+// router.get('/', (req, res) => {
+//     connection.query("SELECT * FROM project", (err, result) => {
+//         if(err) {
+//             res.status(500).send("Error retrieving projects from database");
+//         } else {
+//             res.json(result);
+//         }
+//     })
+// });
+
+router.get('/', async (req, res) => {
+    const [projects] = await findAll();
+    res.json[projects];
+})
 
 router.get('/:id', (req, res) =>  {
     const projectId = req.params.id;
@@ -25,24 +36,40 @@ router.get('/:id', (req, res) =>  {
     })
 });
 
-router.post('/', (req, res) => {
-    const { name, description, picture, date, repo_front, repo_back, deploy_url, category_id } = req.body;
-    connection.query(
-    'INSERT INTO project (name, description, picture, date, repo_front, repo_back, deploy_url, category_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
-    [name, description, picture, date, repo_front, repo_back, deploy_url, category_id],
-    (err, result) => {
-        if (err) {
-            console.log(err)
-            res.status(500).send('Error retrieving project from database');
-        }
-        else 
-        {
-            const id = result.insertId;
-            const createdProject = { id, name, description, picture, date, repo_front, repo_back, deploy_url, category_id };
-            res.status(201).json(createdProject);
-        }
-    }
-)
+// router.post('/', (req, res) => {
+//     const { name, description, picture, date, repo_front, repo_back, deploy_url, category_id } = req.body;
+//     connection.query(
+//     'INSERT INTO project (name, description, picture, date, repo_front, repo_back, deploy_url, category_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+//     [name, description, picture, date, repo_front, repo_back, deploy_url, category_id],
+//     (err, result) => {
+//         if (err) {
+//             console.log(err)
+//             res.status(500).send('Error retrieving project from database');
+//         }
+//         else 
+//         {
+//             const id = result.insertId;
+//             const createdProject = { id, name, description, picture, date, repo_front, repo_back, deploy_url, category_id };
+//             res.status(201).json(createdProject);
+//         }
+//     }
+// )
+// });
+
+// Ici on doit suivre le modèle de la doc officielle de multer
+// à savoir : identifier le fichier (sous son nom de propriété) que l'on va uploader
+// ici nous travaillons sous 'picture'
+router.post('/', upload.single('picture'), async(req, res) => {
+    // dans l'étape de transformation du fichier, nous commençons pour relever le chemin jusqu'au
+    // dossier de réception
+    const [{ insertId: id}] = await insertProject(req.body, req.file.path);
+
+    return res.json({
+        ...req.body,
+        id, 
+        // ici nous avons besoin d'une dernier nom de fichier en date pour le stockage en BDD
+        picture: req.file.filename
+    });
 });
 
 router.put('/:id', (req, res) => {
